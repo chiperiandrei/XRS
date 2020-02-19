754f42c4b35552a9ea07ec4d11f738b51f5d5acd
Andrei Chiperi <andrei.chiperi98@gmail.com>
2020-02-19 15:34:19 +0200

    hosted files

--- xrs-back-end/user-management/src/index.js
+++ xrs-back-end/user-management/src/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+var morgan = require('morgan');
 const app = express();
-const port = 3000;
-app.get('/',(req,res)=>res.send('Hello World!'));
-app.listen(port,()=>console.log(`Example app listening on port ${port}`));
+const port = 4000;
+app.use(morgan('dev'));
+app.use(express.static('public'));
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
