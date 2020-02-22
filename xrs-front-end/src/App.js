f378665bd80ec1e90fca33a20f9d929d74a17bfa
Andrei Chiperi <andrei.chiperi98@gmail.com>
2020-02-22 14:09:52 +0200

    footer.

--- xrs-front-end/src/App.js
+++ xrs-front-end/src/App.js
@@ -3,14 +3,15 @@ import {Switch, Route} from 'react-router-dom';
 import Home from "./components/Home";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
+
 function App() {
-  return (
-      <Switch>
-          <Route exact path='/' component={Home}/>
-          <Route exact path='/login' component={Login}/>
-          <Route exact path='*' component={NotFound}/>
-      </Switch>
-  );
+    return (
+        <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='*' component={NotFound}/>
+        </Switch>
+    );
 }
 
 export default App;
--- xrs-front-end/src/components/Home.js
+++ xrs-front-end/src/components/Home.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import SetUp from './SetUp/SetUp'
 
-const Home = props =>{
+const Home = props => {
     function check_if_is_uploaded() {
         //TODO : api to check if config already exists
     }
-    function upload_config_to_server(){
+
+    function upload_config_to_server() {
         //TODO : upload cfg to server
     }
-    return [<Header companyname="X"/>,<SetUp/>,<Footer datecreated='2020' authorname='Andrei Chiperi'/>]
+
+    return [<Header companyname="X"/>, <SetUp/>, <Footer datecreated='2020' authorname='Andrei Chiperi'/>]
 };
 export default Home;
