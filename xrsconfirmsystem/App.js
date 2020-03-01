fc2a5e45d72a359275844cc648b48d9ab2bb255a
Andrei Chiperi <andrei.chiperi98@gmail.com>
2020-03-01 19:13:53 +0200

    android full controll on main page

--- xrsconfirmsystem/App.js
+++ xrsconfirmsystem/App.js
@@ -1,93 +1,126 @@
-import React from 'react';
-import {View, Text, TouchableOpacity, Alert, Image, Vibration} from 'react-native';
-import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
-import axios from 'axios';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    TextInput,
+    StatusBar,
+    Button,
+    Alert
+} from 'react-native';
+import {
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import axios from 'axios';
+import AppMultiTech from "react-native-nfc-manager/example/AppMultiTech";
+
+const styles = StyleSheet.create({
+    scrollView: {
+        backgroundColor: Colors.lighter,
+    },
+    engine: {
+        position: 'absolute',
+        right: 0,
+    },
+    body: {
+        backgroundColor: Colors.white,
+    },
+    title: {
+        color: 'dodgerblue',
+        textAlign: 'center',
+        fontSize: 30,
+        paddingTop: '2%'
+    },
+    titleForm: {
+        textAlign: 'center',
+        paddingTop: '10%',
+        fontSize: 30,
+        paddingBottom: '10%'
+    },
+    textInput: {
+        borderColor: 'dodgerblue',
+        borderWidth: 1,
+        borderRadius: 1,
+        width: '90%',
+        left: '1.2%',
+        marginBottom:10
+    }
+});
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = {
-            image: "",
-            DURATION: 100
-        };
+            exists: false,
+            company_name: '',
+            created_by: '',
+            email: ''
+        }
     }
+    validate = (email) => {
+        console.log(email);
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (reg.test(text) === false) {
+            this.setState({ email: email });
+            return false;
+        }
+        else {
+            this.setState({ email: email });
+        }
+    };
 
     componentDidMount() {
-        NfcManager.start();
-        NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
-            Vibration.vibrate(this.state.DURATION);
-            Alert.alert('Hello', tag.id, [{
-                text: 'Ask me later', onPress: () => {
-                    axios
-                        .get('https://api.thecatapi.com/v1/images/search')
-                        .then(response => {
-                            this.setState({image: response.data[0].url})
-                        });
-                },
-            }]);
-            NfcManager.unregisterTagEvent().catch(() => 0);
-        });
-    }
-
-    componentWillUnmount() {
-        NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
-        NfcManager.unregisterTagEvent().catch(() => 0);
+        axios.get('http://192.168.43.73:4000/api/files/setup')
+            .then(response => this.setState({
+                exists: response.data.value,
+                company_name: response.data.company_name,
+                created_by: `${response.data.operatorLname} ${response.data.operatorFname}`,
+                email: response.data.email,
+                year: response.data.date_created
+            }))
+            .catch(err => console.log(err))
     }
 
     render() {
-        if (this.state.image == "")
-            this.state.image = 'https://web.uri.edu/visit/files/CAMK-Aerials-169.jpg';
-        return (
-            <View style={{padding: 20}}>
-                <Image
-                    style={{width: 50, height: 50}}
-                    source={require('./assets/img/logo.png')}/>
-                <Text style={{color: 'dodgerblue', fontFamily: 'Squada One, cursive', fontWeight: 'bold'}}>XRS Confirm
-                    Products</Text>
-                <TouchableOpacity
-                    style={{
-                        padding: 10,
-                        width: 200,
-                        margin: 20,
-                        borderWidth: 1,
-                        borderColor: 'black',
-                    }}
-                    onPress={this._test}>
-                    <Text>Salut</Text>
-                </TouchableOpacity>
+        return <>
+            <StatusBar barStyle="dark-content"/>
+            <SafeAreaView>
+                <View><Text style={styles.title}>Welcome to {this.state.company_name}</Text>
+                </View>
+                <View><Text style={styles.titleForm}>Please log in into operator admin area</Text></View>
+                <View>
+                    <Text>Email</Text>
+                    <TextInput style={styles.textInput} placeholder="Email..."
+                               onChangeText={(text) => this.validate(text)}/>
 
-                <TouchableOpacity
-                    style={{
-                        padding: 10,
-                        width: 200,
-                        margin: 20,
-                        borderWidth: 1,
-                        borderColor: 'black',
-                    }}
-                    onPress={this._cancel}>
-                    <Text>Cancel Test</Text>
-                </TouchableOpacity>
-                <Text>{this.state.image}</Text>
-                <Image source={{uri: this.state.image}}
-                       style={{width: 200, height: 200}}
-                />
-            </View>
-        );
-    }
+                    <Text>Password</Text>
+                    <TextInput style={styles.textInput} secureTextEntry={true} placeholder="Password..."/>
+                    <Button
+                        title="Login"
+                        onPress={() => Alert.alert('Hello', 'Login form bro', [{
+                            text: 'Close'
+                        }])}
+                    />
+                </View>
+                <View><Text style={styles.title}>Or place your ACCESS CARD on back of the phone</Text></View>
+            </SafeAreaView>
+        </>
 
-    _cancel = () => {
-        NfcManager.unregisterTagEvent().catch(() => 0);
     };
 
-    _test = async () => {
-        try {
-            await NfcManager.registerTagEvent();
-        } catch (ex) {
-            console.warn('ex', ex);
-            NfcManager.unregisterTagEvent().catch(() => 0);
-        }
-    };
 }
 
 export default App;
---
+++ xrsconfirmsystem/components/Home.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Alert, Image, Vibration} from 'react-native';
+import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
+import axios from 'axios';
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: "",
+            DURATION: 100,
+            NFC_ID:""
+        };
+    }
+
+    componentDidMount() {
+        NfcManager.start();
+        NfcManager.setEventListener(NfcEvents.DiscoverTag, tag => {
+            Vibration.vibrate(this.state.DURATION);
+            Alert.alert('Hello', tag.id, [{
+                text: 'Ask me later', onPress: () => {
+                    axios
+                        .get('https://api.thecatapi.com/v1/images/search')
+                        .then(response => {
+                            this.setState({image: response.data[0].url})
+                        });
+                },
+            }]);
+            NfcManager.unregisterTagEvent().catch(() => 0);
+        });
+    }
+
+    componentWillUnmount() {
+        NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+        NfcManager.unregisterTagEvent().catch(() => 0);
+    }
+
+    render() {
+        if (this.state.image == "")
+            this.state.image = 'https://web.uri.edu/visit/files/CAMK-Aerials-169.jpg';
+        return (
+            <View style={{padding: 20}}>
+                <Image
+                    style={{width: 50, height: 50}}
+                    source={require('../assets/img/logo.png')}/>
+                <Text style={{color: 'dodgerblue', fontFamily: 'Squada One, cursive', fontWeight: 'bold'}}>XRS Confirm
+                    Products</Text>
+                <TouchableOpacity
+                    style={{
+                        padding: 10,
+                        width: 200,
+                        margin: 20,
+                        borderWidth: 1,
+                        borderColor: 'black',
+                    }}
+                    onPress={this._test}>
+                    <Text>Salut</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        padding: 10,
+                        width: 200,
+                        margin: 20,
+                        borderWidth: 1,
+                        borderColor: 'black',
+                    }}
+                    onPress={this._cancel}>
+                    <Text>Cancel Test</Text>
+                </TouchableOpacity>
+                <Text>{this.state.image}</Text>
+                <Image source={{uri: this.state.image}}
+                       style={{width: 200, height: 200}}
+                />
+            </View>
+        );
+    }
+
+    _cancel = () => {
+        NfcManager.unregisterTagEvent().catch(() => 0);
+    };
+
+    _test = async () => {
+        try {
+            await NfcManager.registerTagEvent();
+        } catch (ex) {
+            console.warn('ex', ex);
+            NfcManager.unregisterTagEvent().catch(() => 0);
+        }
+    };
+}
+
+export default App;
